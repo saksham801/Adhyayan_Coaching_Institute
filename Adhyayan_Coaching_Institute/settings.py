5040dfa214d715134aea9dd7443f400f36e95357
Saksham <dubeysaksham801@gmail.com>
2025-05-23 01:11:31 +0530

    Initial Commit

--- Adhyayan_Coaching_Institute/settings.py
+++ Adhyayan_Coaching_Institute/settings.py
@@ -34,7 +34,7 @@ RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
 
 RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
 
-ALLOWED_HOSTS = ['localhost']
+ALLOWED_HOSTS = ['localhost','https://adhyayan-coaching-institute-2.onrender.com']
 if RENDER_EXTERNAL_HOSTNAME:
     ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
